74da74c58beaf00250adc5f11f452a88739e1411
ctq3103 <56945989+ctq3103@users.noreply.github.com>
2020-06-01 20:31:25 +0700

    .

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchGlobal } from '../actions';
 import _ from 'lodash';
@@ -9,51 +9,57 @@ import CardItem from './CardItem';
 import Loading from './Loading';
 
 class Global extends Component {
-    componentDidMount() {
-        this.props.fetchGlobal();
-    }
-    
-    render() {
-        const {TotalConfirmed, TotalDeaths, TotalRecovered, NewConfirmed, NewDeaths, NewRecovered } = this.props.globalData;
-        if (this.props.loading) return <Loading />
-        return (
-            <section className="cards">
-                {!(_.isEmpty(this.props.globalData)) &&
-                <>
-                    <CardItem 
-                        cardTitle='Worldwide'
-                        dataValue={numReformat(TotalConfirmed)}
-                        dataTitle='TOTAL CONFIRMED'
-                        changeValue={numReformat(NewConfirmed)}
-                        changeClassName={NewConfirmed > 0 ? "change-negative" : "change"}
-                    />
-                    <CardItem 
-                        cardTitle='Worldwide'
-                        dataValue={numReformat(TotalRecovered)}
-                        dataTitle='TOTAL RECORVERED'
-                        changeValue={numReformat(NewRecovered)}
-                        changeClassName={NewRecovered > 0 ? "change" : "change-negative"}
-                    />
-                    <CardItem 
-                        cardTitle='Worldwide'
-                        dataValue={numReformat(TotalDeaths)}
-                        dataTitle='TOTAL RECORVERED'
-                        changeValue={numReformat(NewDeaths)}
-                        changeClassName={NewDeaths > 0 ? "change-negative" : "change"}
-                    />
-                </>
-                }
-                </section>
-                )
-        }
+	componentDidMount() {
+		this.props.fetchGlobal();
+	}
+
+	render() {
+		const {
+			TotalConfirmed,
+			TotalDeaths,
+			TotalRecovered,
+			NewConfirmed,
+			NewDeaths,
+			NewRecovered,
+		} = this.props.globalData;
+		if (this.props.loading) return <Loading />;
+		return (
+			<section className="cards">
+				{!_.isEmpty(this.props.globalData) && (
+					<>
+						<CardItem
+							cardTitle="Worldwide"
+							dataValue={numReformat(TotalConfirmed)}
+							dataTitle="TOTAL CONFIRMED"
+							changeValue={numReformat(NewConfirmed)}
+							changeClassName={NewConfirmed > 0 ? 'change-negative' : 'change'}
+						/>
+						<CardItem
+							cardTitle="Worldwide"
+							dataValue={numReformat(TotalRecovered)}
+							dataTitle="TOTAL RECORVERED"
+							changeValue={numReformat(NewRecovered)}
+							changeClassName={NewRecovered > 0 ? 'change' : 'change-negative'}
+						/>
+						<CardItem
+							cardTitle="Worldwide"
+							dataValue={numReformat(TotalDeaths)}
+							dataTitle="TOTAL DEATHS"
+							changeValue={numReformat(NewDeaths)}
+							changeClassName={NewDeaths > 0 ? 'change-negative' : 'change'}
+						/>
+					</>
+				)}
+			</section>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-    return { 
-        globalData: state.global,
-        loading: state.async.loading }
-  }
+const mapStateToProps = (state) => {
+	return {
+		globalData: state.global,
+		loading: state.async.loading,
+	};
+};
 
-export default connect(mapStateToProps, {fetchGlobal})(Global);
-
-
+export default connect(mapStateToProps, { fetchGlobal })(Global);
